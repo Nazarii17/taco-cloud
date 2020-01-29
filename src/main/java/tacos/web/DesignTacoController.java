5cec3afaa5cf2206769f6dd98e890d475e813238
Nazarii Tkachuk <nazarii.tkachuk.28@gmail.com>
2020-01-29 15:53:32 +0200

    added WebConfig

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,18 +1,21 @@
+// tag::head[]
 package tacos.web;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import lombok.extern.slf4j.Slf4j;
-//import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
@@ -21,8 +24,11 @@ import tacos.Taco;
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
-    @GetMapping
-    public String showDesignForm(Model model) {
+
+//end::head[]
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -35,12 +41,63 @@ public class DesignTacoController {
                 new Ingredient("SLSA", "Salsa", Type.SAUCE),
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
+
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
+    }
+
+    //tag::showDesignForm[]
+    @GetMapping
+    public String showDesignForm(Model model) {
         model.addAttribute("design", new Taco());
         return "design";
     }
+
+//end::showDesignForm[]
+
+/*
+//tag::processDesign[]
+  @PostMapping
+  public String processDesign(Design design) {
+    // Save the taco design...
+    // We'll do this in chapter 3
+    log.info("Processing design: " + design);
+
+    return "redirect:/orders/current";
+  }
+
+//end::processDesign[]
+ */
+
+    //tag::processDesignValidated[]
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+
+//end::processDesignValidated[]
+
+    //tag::filterByType[]
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+//end::filterByType[]
+// tag::foot[]
 }
+// end::foot[]
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,6 +2,7 @@
 package tacos.web;
 
 import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,20 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
 
-
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
-  @GetMapping("/current")
-  public String orderForm(Model model) {
-    model.addAttribute("order", new Order());
-    return "orderForm";
-  }
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
 
-  @PostMapping
-  public String processOrder(Order order) {
-    log.info("Order submitted: " + order);
-    return "redirect:/";
-  }
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
 }
+
