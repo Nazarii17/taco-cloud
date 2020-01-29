e030c2f036d450743bb87df51a77fd7c945c41ad
Nazarii Tkachuk <nazarii.tkachuk.28@gmail.com>
2020-01-29 15:07:41 +0200

    updated app

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,8 +1,10 @@
 package tacos.web;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
-import tacos.Taco;
+//import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Taco;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
